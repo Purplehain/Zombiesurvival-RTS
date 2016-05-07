c008ee16c34b764a8a215c0173e706d31237ccd8
Purplehain <max@wir.cc>
2016-05-07 02:35:43 +0200

    a
    b

---
+++ Assets/scripts/WorldGenerator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (MeshFilter), typeof(MeshRenderer))]
+public class WorldGenerator : MonoBehaviour {
+
+    public int sizeX, sizeY;
+
+    void Awake( ) {
+        GenerateGrid();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private Vector3[] vertices;
+    void GenerateGrid( ) {
+        vertices = new Vector3[(sizeX + 1) * (sizeY + 1)];
+        for (int i = 0, y = 0 ; y <= sizeY ; y++ ) {
+            for (int x = 0 ; x <= sizeX ;  x++, i++ ) {
+                vertices[i] = new Vector3( x , y );
+            }
+        }
+    }
+    private void OnDrawGizmos( ) {
+        if(vertices == null ) {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for(int i = 0 ; i < vertices.Length ; i++ ) {
+            Gizmos.DrawSphere( vertices[i] , 0.1f );
+        }
+    }
+
+    void GenerateStreets( ) {
+
+    } 
+
+    void GenerateBuildings( ) {
+
+    }
+
+    void GenerateRessources( ) {
+
+    }
+
+}
